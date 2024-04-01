02cd4d356dc1b533e4c25708da76e0f75ffebf69
Bucs0 <146287084+Bucs0@users.noreply.github.com>
2024-04-01 16:58:50 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('SCP_STORIES_FOR_KIDS').addEventListener('click', function() {
+    window.location.href = 'SCP_STORIES_FOR_KIDS/chapter1.html';
+});
+
+document.getElementById('STICK_MAN_STORY').addEventListener('click', function() {
+    window.location.href = 'STICK_MAN_STORY/chapter1.html';
+});
+
+document.getElementById('MONKEY_AND_THE_DOG').addEventListener('click', function() {
+    window.location.href = 'MONKEY_AND_THE_DOG/chapter1.html';
+});
